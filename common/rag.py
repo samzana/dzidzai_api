e89982f3f4c3721393246521872c5106dbfe7828
samzana <samuelzana18@gmail.com>
2024-07-18 20:49:18 +0200

    include environment variables

--- common/rag.py
+++ common/rag.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Retrieve the OpenAI API key from the environment variable
-openai_api_key = "sk-proj-K3vV46dsYdKJ3M4v5TRJT3BlbkFJn5JnugBxheXoyckV0JBe"
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 if not openai_api_key:
     raise ValueError("OpenAI API key not found. Please set it in the .env file or check the key.")
